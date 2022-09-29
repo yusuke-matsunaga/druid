9a161371392e15ade4a5de68e71e91959e4ddd98
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-09-30 07:52:18 +0900

    py_druid 作成中

--- c++-src/py_druid/PyVal3.cc
+++ c++-src/py_druid/PyVal3.cc
@@ -24,6 +24,11 @@ struct Val3Object
   Val3 mVal;
 };
 
+// Python 用のタイプ定義
+PyTypeObject Val3Type = {
+  PyVarObject_HEAD_INIT(nullptr, 0)
+};
+
 // 定数0
 PyObject* Val3_0 = nullptr;
 
@@ -37,28 +42,6 @@ PyObject* Val3_X = nullptr;
 PyObject*
 Val3_new(
   PyTypeObject* type,
-  PyObject* args,
-  PyObject* kwds
-)
-{
-  auto self = reinterpret_cast<Val3Object*>(type->tp_alloc(type, 0));
-  // 必要なら self の初期化を行う．
-  return reinterpret_cast<PyObject*>(self);
-}
-
-// 終了関数
-void
-Val3_dealloc(
-  Val3Object* self
-)
-{
-  Py_TYPE(self)->tp_free(reinterpret_cast<PyObject*>(self));
-}
-
-// 初期化関数(__init__()相当)
-int
-Val3_init(
-  Val3Object* self,
   PyObject* args,
   PyObject* Py_UNUSED(kwds)
 )
@@ -75,58 +58,72 @@ Val3_init(
   //     "?" -> Val3::_X
   // それ以外は TypeError
 
-  PyObject* obj;
-  if ( !PyArg_ParseTuple(args, "O", &obj) ) {
-    return -1;
+  PyObject* obj1;
+  if ( !PyArg_ParseTuple(args, "O", &obj1) ) {
+    return nullptr;
   }
-  if ( PyLong_Check(obj) ) {
-    long val = PyLong_AsLong(obj);
+  PyObject* val3_obj = nullptr;
+  if ( PyLong_Check(obj1) ) {
+    long val = PyLong_AsLong(obj1);
     if ( val == 0 ) {
-      self->mVal = Val3::_0;
+      val3_obj = Val3_0;
     }
     else if ( val == 1 ) {
-      self->mVal = Val3::_1;
+      val3_obj = Val3_1;
     }
     else {
       // エラー
       PyErr_SetString(PyExc_TypeError, "argument 1 must be 0 or 1");
-      return -1;
+      return nullptr;
     }
   }
   else {
     const char* val_str = nullptr;
     if ( !PyArg_ParseTuple(args, "s", &val_str) ) {
-      return -1;
+      return nullptr;
     }
     if ( strcmp(val_str, "0") == 0 ) {
-      self->mVal = Val3::_0;
+      val3_obj = Val3_0;
     }
     else if ( strcmp(val_str, "1") == 0 ) {
-      self->mVal = Val3::_1;
+      val3_obj = Val3_1;
     }
     else if ( strcmp(val_str, "x") == 0 ||
 	      strcmp(val_str, "X") == 0 ||
 	      strcmp(val_str, "?") == 0 ) {
-      self->mVal = Val3::_X;
+      val3_obj = Val3_X;
     }
     else {
       // エラー
       PyErr_SetString(PyExc_TypeError,
 		      "argument 1 must be \"0\", \"1\", \"x\", \"X\" or \"?\"");
-      return -1;
+      return nullptr;
     }
   }
-  return 0;
+  Py_INCREF(val3_obj);
+  return val3_obj;
 }
 
-// str() 関数
-PyObject*
-Val3_str(
+// 終了関数
+void
+Val3_dealloc(
   PyObject* self
 )
 {
+  // auto val3_obj = reinterpret_cast<Val3Object*>(obj);
+  // 必要なら val3_obj->mVal の終了処理を行う．
+  Py_TYPE(self)->tp_free(self);
+}
+
+// repr() 関数
+PyObject*
+Val3_repr(
+  PyObject* self
+)
+{
+  auto val3_obj = reinterpret_cast<Val3Object*>(self);
   const char* tmp_str = nullptr;
-  switch ( reinterpret_cast<Val3Object*>(self)->mVal ) {
+  switch ( val3_obj->mVal ) {
   case Val3::_X: tmp_str = "X"; break;
   case Val3::_0: tmp_str = "0"; break;
   case Val3::_1: tmp_str = "1"; break;
@@ -139,10 +136,24 @@ PyMethodDef Val3_methods[] = {
   {nullptr, nullptr, 0, nullptr}
 };
 
-// Python 用のタイプ定義
-PyTypeObject Val3Type = {
-  PyVarObject_HEAD_INIT(nullptr, 0)
-};
+// 定数オブジェクトの登録
+bool
+reg_val3(
+  PyObject* m,
+  PyObject*& obj,
+  const char* name,
+  Val3 val
+)
+{
+  obj = Val3Type.tp_alloc(&Val3Type, 0);
+  auto val3_obj = reinterpret_cast<Val3Object*>(obj);
+  val3_obj->mVal = val;
+  Py_INCREF(obj);
+  if ( PyModule_AddObject(m, name, obj) < 0 ) {
+    return false;
+  }
+  return true;
+}
 
 END_NONAMESPACE
 
@@ -160,9 +171,8 @@ PyInit_Val3(
   Val3Type.tp_flags = Py_TPFLAGS_DEFAULT;
   Val3Type.tp_doc = PyDoc_STR("Val3 objects");
   Val3Type.tp_methods = Val3_methods;
-  Val3Type.tp_init = reinterpret_cast<initproc>(Val3_init);
   Val3Type.tp_new = Val3_new;
-  Val3Type.tp_str = Val3_str;
+  Val3Type.tp_repr = Val3_repr;
   if ( PyType_Ready(&Val3Type) < 0 ) {
     return false;
   }
@@ -170,34 +180,30 @@ PyInit_Val3(
   Py_INCREF(&Val3Type);
   if ( PyModule_AddObject(m, "Val3", reinterpret_cast<PyObject*>(&Val3Type)) < 0 ) {
     Py_DECREF(&Val3Type);
-    return false;
+    goto error;
   }
 
-  Val3_0 = Val3_new(&Val3Type, nullptr, nullptr);
-  reinterpret_cast<Val3Object*>(Val3_0)->mVal = Val3::_0;
-  Py_INCREF(Val3_0);
-  if ( PyModule_AddObject(m, "Val3_0", Val3_0) < 0 ) {
-    Py_DECREF(Val3_0);
-    return false;
+  if ( !reg_val3(m, Val3_0, "Val3_0", Val3::_0) ) {
+    goto error;
   }
 
-  Val3_1 = Val3_new(&Val3Type, nullptr, nullptr);
-  reinterpret_cast<Val3Object*>(Val3_1)->mVal = Val3::_1;
-  Py_INCREF(Val3_1);
-  if ( PyModule_AddObject(m, "Val3_1", Val3_1) < 0 ) {
-    Py_DECREF(Val3_1);
-    return false;
+  if ( !reg_val3(m, Val3_1, "Val3_1", Val3::_1) ) {
+    goto error;
   }
 
-  Val3_X = Val3_new(&Val3Type, nullptr, nullptr);
-  reinterpret_cast<Val3Object*>(Val3_X)->mVal = Val3::_X;
-  Py_INCREF(Val3_X);
-  if ( PyModule_AddObject(m, "Val3_X", Val3_X) < 0 ) {
-    Py_DECREF(Val3_X);
-    return false;
+  if ( !reg_val3(m, Val3_X, "Val3_X", Val3::_X) ) {
+    goto error;
   }
 
   return true;
+
+ error:
+
+  Py_XDECREF(Val3_0);
+  Py_XDECREF(Val3_1);
+  Py_XDECREF(Val3_X);
+
+  return false;
 }
 
 bool
@@ -207,7 +213,7 @@ Val3_from_PyObj(
 )
 {
   if ( !Py_IS_TYPE(obj, &Val3Type) ) {
-    PyErr_SetString(PyExc_ValueError, "object is not a Val3 type");
+    PyErr_SetString(PyExc_TypeError, "object is not a Val3 type");
     return false;
   }
   auto val3_obj = reinterpret_cast<Val3Object*>(obj);
@@ -220,9 +226,10 @@ PyObj_from_Val3(
   Val3 val
 )
 {
-  auto val3_obj = Val3_new(&Val3Type, nullptr, nullptr);
-  reinterpret_cast<Val3Object*>(val3_obj)->mVal = val;
-  return val3_obj;
+  auto obj = Val3_new(&Val3Type, nullptr, nullptr);
+  auto val3_obj = reinterpret_cast<Val3Object*>(obj);
+  val3_obj->mVal = val;
+  return obj;
 }
 
 END_NAMESPACE_DRUID
