8eba48676981007c78a8450e78b9b0482b2f660c
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2023-02-16 23:33:21 +0900

    tests/test-programs/read_iscas89 を追加し忘れていた．

---
+++ tests/test-programs/read_iscas89/read_iscas89_test.cc
@@ -0,0 +1,39 @@
+
+/// @file read_iscas89_test.cc
+/// @brief read_iscas89_test の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2023 Yusuke Matsunaga
+/// All rights reserved.
+
+#include "TpgNetwork.h"
+
+
+BEGIN_NAMESPACE_DRUID
+
+int
+read_iscas89_test(
+  int argc,
+  char** argv
+)
+{
+  if ( argc != 2 ) {
+    return 10;
+  }
+  auto network = TpgNetwork::read_iscas89(argv[1]);
+  print_network(cout, network);
+
+  return 0;
+}
+
+END_NAMESPACE_DRUID
+
+
+int
+main(
+  int argc,
+  char** argv
+)
+{
+  return nsYm::nsDruid::read_iscas89_test(argc, argv);
+}
